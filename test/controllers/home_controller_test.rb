4a23189deec06b15a49bd855294d3665644e3041
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-11-06 22:31:16 -0600

    remove dumb test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
-    get home_welcome_url
-    assert_response :success
   end
 
 end
